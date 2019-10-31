3cc5c5db1cf5b4d720356a48e11d47f268701d09
Alex' Le Coq <alexandre.lcoq@gmail.com>
2019-10-31 11:48:51 +0100

    Final Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,12 @@ import './Footer.css'
 
 function Footer () {
     return (
-    <footer className="Footer">
-                <p className = "Footer-text"> 
-                    Directed by Stanislas VEGA,<br/>Bruno HOARAU,<br/> Rémy POTTIER <br/>et Alex' LE COQ
-                </p>
-                <img src="https://i.imgur.com/AN7Y54W.png" alt="Footer img" className="Footer-img"/>
-                <img src="https://i.imgur.com/qGea8gf.png" alt="Footer Joke" className="Footer-logo"/>
+        <footer className="Footer">
+            <p className = "Footer-text"> 
+                Directed by Stanislas VEGA,<br/>Bruno HOARAU,<br/> Rémy POTTIER <br/>et Alex' LE COQ
+            </p>
+            <img src="https://i.imgur.com/AN7Y54W.png" alt="Footer img" className="Footer-img"/>
+            <img src="https://i.imgur.com/qGea8gf.png" alt="Footer Joke" className="Footer-logo"/>
         </footer>
     )
 }
