09795c2e43348e3b4b929dd4519d02807085978c
Alex' Le Coq <alexandre.lcoq@gmail.com>
2019-10-31 11:50:39 +0100

    fix for PR

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ class App extends Component {
     constructor(props) {
         super(props)
     }
-    slide(){
-      console.log('hello')
-      let index = []
-      for(let i = 1; i<83;i++){
-        index.push(i)
-      }
-      console.log(index)
-      return index.map(x=> {
-      return <Slide id={x} />
-    })
-    }
     render(){
       return(
         <div className="App">
