d23357b09b9a30c005962a91dadae70e5dd01af5
Hoarau Bruno <bwouno@icloud.com>
2019-10-31 14:29:02 +0100

    finish responsive of slide component

--- src/App.js
+++ src/App.js
@@ -14,14 +14,21 @@ class App extends Component {
       id:[17,67,66,68,71],
       image:["SAW","Psychose","Shining","dawn-of-the-dead","Nosferatu"],
       music:["Saw Theme Song- Hello Zepp","Theme from Psycho (1960) - Shower Scene HQ","Shining","dawn of the dead main theme","Hans Erdmann - Nosferatu [Main Theme] (1922)"],
+      description:[
+        "On tous un pote sadique, non ? Jigsaw, c’est un peu l’équivalent de cet ami en question mais qui aurait mal tourné. Lancés par James Wan (Indisious, Fast & Furious 7, Aquaman), les long-métrages mettant en scène des Escape Games sanglants auront donné vie à un antagoniste très marquant dans son design rappelant qu’il ne faut jamais faire confiance à des poupées.",
+        "Psychose a marqué l’Histoire et l’Horreur avec de grands H. C’est l’épreuve du temps qui a porté Psychose au panthéon des plus grands long-métrages psychologiques avec son intrigue au cœur du motel géré par le dérangé, Norman Bates. En plus d’avoir des scènes cultes qui auront servi de points de référence pour les cinéastes du monde entier, le film atteint un niveau de tension presque inégalé à l’époque sur le grand écran.",
+        "La magie de la plume de Stephen King mélangée au génie de la réalisation de Stanley Kubrick ne pouvait mener qu’à un projet qui resterait ancré dans l’inconscient collectif, Shining. Pour autant la personnalité forte du réalisateur de 2001 l’Odyssée de l’Espace va le pousser à l’éloigner du livre original pour offrir sa propre interprétation de l’œuvre. Si Stephen King apprécie moyennement, il en reste un film culte qui rayonne par l’interprétation de Jack Torrance par un jeune Jack Nicholson..",
+        "George A. Romero est maître pour manier les règles du cinéma et du fantastique afin d’y glisser des messages idéologiques forts. Dawn of the Dead est une révolution qui joue de son sujet pour proposer une critique acerbe du capitalisme. Il juge en effet pertinent de présenter ses morts-vivants comme des êtres vide de sens, cherchant seulement à se nourrir, à l’image de notre société consumériste. L’image va d’ailleurs plus loin en plaçant son intrigue directement dans un centre commercial. ",
+        "Nosferatu n’est autre qu’une adaptation du roman britannique classique Dracula de Bram Stoker mais pas vraiment autorisée. Si le film est classique, c’est à travers le procès qui va suivre sa sortie, intentée par la femme de l’écrivain, Florence Stoker. Résultat, la justice oblige le studio à détruire toutes les copies existantes du long métrage. Mais un vampire n’est jamais vraiment mort et le film réapparaît au grand jour en 1937, après la mort de Florence Stoker."
+      ],
       index:0,
       director:null,
       posterUrl:null,
       title:null,
       year:null,
       showInfo:false,
-      musicPlay:true,
-      showHeader: true
+      musicPlay:false,
+      showHeader: false
     }
   }
   componentDidMount(){
@@ -90,6 +97,7 @@ class App extends Component {
                 year={this.state.year} incId={this.incId} 
                 decId={this.decId}
                 posterUrl={this.state.posterUrl}
+                desc={this.state.description[this.state.index]}
               />
             :<p>LOADING</p>
           }
--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -17,7 +17,7 @@ class Slide extends Component {
                         <i className="fas fa-chevron-left"></i>
                 </button>
                 <img 
-                    className={`poster ${this.props.showInfo ? "blur":""}`} 
+                    className="poster" 
                     src={`https://raw.githubusercontent.com/RemyPottierFr/hackaton-halloween/feature/image/${this.props.image}.png`} 
                     alt={this.props.id} />
                 <button 
@@ -26,20 +26,16 @@ class Slide extends Component {
                         <i className="fas fa-info-circle"></i>
                 </button>
                 <div className={this.props.showInfo ? "info--show":"info--hidden"}>
-                        <p className="info__title">{this.props.title}</p>
+                        <p className="info__title">{this.props.title.replace(/_/gi," ")}</p>
                         <div className="synopsis"> 
                                 <img 
                                     className="image__poster"
                                     src={`${this.props.posterUrl}`}
                                     alt={this.props.id} 
                                 />
-                            <div className="synopsis__left">
-                                <p className="info__director">{this.props.director}, {this.props.year}</p>
-                                <p className="info__desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, corrupti.
-                                    Ratione, totam amet nisi distinctio excepturi voluptate sapiente neque quibusdam vitae dignissimos sequi
-                                    eum assumenda illum. Nam, 
-                                    libero pariatur culpa ipsa tenetur quo, accusantium enim illum, ex iste quisquam molestias esse fugiat 
-                                    quasi voluptatem ratione officia corporis nostrum optio vitae.
+                            <div className="synopsis__right">
+                                <p className="info__director">{this.props.director.replace(/_/gi," ")}, {this.props.year}</p>
+                                <p className="info__desc">{this.props.desc}
                                 </p>
                             </div>
                         </div>
